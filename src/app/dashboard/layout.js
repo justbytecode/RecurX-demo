7661e249b1a9e7268e4be0ecb95b3ef133fda2f9
shub12388y <spl.sp999@gmail.com>
2025-07-20 15:56:26 +0530

    added the wallet provider

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -7,6 +7,7 @@ import { useTheme } from "../../context/themeContext";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import { MessageCircleWarning } from "lucide-react";
 
+
 function Layout({ children }) {
   const { themeClasses } = useTheme();
 
